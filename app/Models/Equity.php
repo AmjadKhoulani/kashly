c9ac7f8d50b73e04d185ab34a731e47ace8653b9
Amjad Khoulani <amjadkhoulani@gmail.com>
2026-05-12 00:26:32 +0300

    Fix: Defined missing Eloquent relationships in Partner, Equity, and InvestmentFund models

--- app/Models/Equity.php
+++ app/Models/Equity.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equity extends Model
 {
-    //
+    protected $casts = [
+        'percentage' => 'decimal:2',
+        'amount' => 'decimal:2',
+    ];
+
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
+
+    public function equitable()
+    {
+        return $this->morphTo();
+    }
 }
