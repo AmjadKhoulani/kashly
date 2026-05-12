35ff50ca66e61b16351eecd48c6b575e0a08844d
Amjad Khoulani <amjadkhoulani@gmail.com>
2026-05-12 10:00:17 +0300

    Fix: Defined transactionable relationship and updated fund show UI

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,5 +8,16 @@ class Transaction extends Model
 {
     protected $casts = [
         'transaction_date' => 'datetime',
+        'amount' => 'decimal:2',
     ];
+
+    public function transactionable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
