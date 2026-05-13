687f4e31c9ae51b65a02bd152a4236680e49a1fd
Amjad Khoulani <amjadkhoulani@gmail.com>
2026-05-13 16:22:20 +0300

    Full redesign of Landing Page and cleanup of root index.php

--- index.php
+++ index.php
@@ -7,11 +7,4 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-try {
-    require __DIR__.'/public/index.php';
-} catch (\Throwable $e) {
-    echo "<h1>Laravel Startup Error</h1>";
-    echo "<p><strong>Message:</strong> " . $e->getMessage() . "</p>";
-    echo "<p><strong>File:</strong> " . $e->getFile() . " on line " . $e->getLine() . "</p>";
-    echo "<pre>" . $e->getTraceAsString() . "</pre>";
-}
+require __DIR__.'/public/index.php';
