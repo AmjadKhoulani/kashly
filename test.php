35f8bb3e147bbe8632c2eda0576551e691a1069f
Amjad Khoulani <amjadkhoulani@gmail.com>
2026-05-13 16:20:45 +0300

    Removed test files

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-echo "PHP is working. Version: " . phpversion() . "<br><br>";
-echo "Loaded Extensions: <pre>" . implode(", ", get_loaded_extensions()) . "</pre>";
