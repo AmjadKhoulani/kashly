dc1fcc6696b9f99a40edd503cbcba2e72d33ef96
Amjad Khoulani <amjadkhoulani@gmail.com>
2026-05-13 16:08:21 +0300

    Updated test.php to show extensions

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo "PHP is working. Version: " . phpversion();
+echo "PHP is working. Version: " . phpversion() . "<br><br>";
+echo "Loaded Extensions: <pre>" . implode(", ", get_loaded_extensions()) . "</pre>";
